50dbb840de1b03c76c37b1b7368021988a697dd3
Dinesh Pundkar <dinesh.pundkar@gmail.com>
2025-07-27 22:19:11 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,26 +12,3 @@ output "bucket_region" {
   description = "Region of the created S3 bucket"
   value       = aws_s3_bucket.demo_bucket.region
 }
-
-output "bucket_domain_name" {
-  description = "Domain name of the S3 bucket"
-  value       = aws_s3_bucket.demo_bucket.bucket_domain_name
-}
-
-output "demo_objects" {
-  description = "Demo objects created in the bucket"
-  value = {
-    demo_file   = aws_s3_object.demo_object.key
-    config_file = aws_s3_object.config_object.key
-  }
-}
-
-output "bucket_features" {
-  description = "Enabled bucket features"
-  value = {
-    versioning_enabled = aws_s3_bucket_versioning.demo_bucket_versioning.versioning_configuration[0].status
-    encryption_enabled = true
-    lifecycle_enabled  = var.enable_lifecycle
-    public_access_blocked = true
-  }
-}
